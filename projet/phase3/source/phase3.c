88204a68c139ba7d40ba93147e9ba10d1830abf3
etu45639@henallux.be <compteHENA0>
2022-04-28 19:27:18 +0200

    phase 3 - end.1

--- projet/phase3/source/phase3.c
+++ projet/phase3/source/phase3.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define FI_MODEL_PATH "../fimodel.csv"
 #define TRAIN_SET_PATH "../../phase1/trainSet.csv"
 
+#define TIME_FOR_A_MIN 600
+
 typedef struct dataDixiemeSeconde DataDixiemeSeconde;
 struct dataDixiemeSeconde{
-    double totVac;
-    double totCarreVacc;
-    int nbLigne;
+    double totVacc;
+    double totSquareVacc;
+    int nbLine;
 };
+
+void initFiModel(FILE* fiModel);
 
 void main(void){
     FILE* fiModel;
@@ -17,11 +22,101 @@ void main(void){
 
     fopen_s(&trainSet, TRAIN_SET_PATH, "r");
     fopen_s(&fiModel, FI_MODEL_PATH, "w+");
+    initFiModel(fiModel);
 
     if(trainSet == NULL || fiModel == NULL){
         printf("erreur ouverture des fichier\n");
     }
     else{
-        printf("000");
+    //utilisé pour la moyenne global
+        //nombre de ligne pour le mouvement
+        double totMovement = 0;
+        //somme des vacc pour ce mouvement
+        int totNbVacc = 0;
+
+        char movementType[20];
+        char currentMovement[20];
+        double vacc;
+        char line[10000];
+        char* token;
+        char* nextToken;
+
+        //contient les Vacc d'un mouvement pour chaque dixieme de seconde
+        DataDixiemeSeconde data[TIME_FOR_A_MIN];
+        for(int i = 0; i < TIME_FOR_A_MIN; i++){
+            data[i].totVacc = 0;
+            data[i].totSquareVacc = 0;
+            data[i].nbLine = 0;
+        }
+
+
+        //supprime la ligne des titres
+        fgets(line, sizeof(line), trainSet);
+        //récupere la première ligne de données
+        fgets(line, sizeof(line), trainSet);
+
+        while(!feof(trainSet)){
+        //lis le titre du mouvement en cours
+            token = strtok_s(line, ";", &nextToken);
+            strcpy(currentMovement, token);
+
+            do{
+                //passe la colonne d'index et du genre
+                token = strtok_s(NULL, ";", &nextToken);
+                token = strtok_s(NULL, ";", &nextToken);
+
+                int iColumn = 0;
+                //récupere les Vacc de la ligne
+                token = strtok_s(NULL, ";", &nextToken);
+                while(iColumn < TIME_FOR_A_MIN && token != NULL){
+                    sscanf_s(token, "%lf", &vacc);
+                    data[iColumn].totVacc += vacc;
+                    data[iColumn].totSquareVacc += vacc * vacc;
+                    data[iColumn].nbLine++;
+
+                    totNbVacc += vacc;
+                    totMovement++;
+                    
+                    iColumn++;
+                    token = strtok_s(NULL, ";", &nextToken);
+                }
+            //passe a la ligne suivante
+                fgets(line, sizeof(line), trainSet);
+                token = strtok_s(line, ";", &nextToken);
+                strcpy(movementType, token);
+    printf("%s", currentMovement);
+            }while(strcmp(movementType, currentMovement) == 0 && !feof(trainSet));
+
+        //ecriture des vacc dans fiModel
+            fprintf(fiModel, "%c", '\n');
+            //ecriture des moyennes
+            fprintf(fiModel, "%s;", currentMovement);
+            for(int i = 0; i < TIME_FOR_A_MIN; i++)
+                fprintf(fiModel, "%lf;", data[i].totVacc / data[i].nbLine);
+            fprintf(fiModel, "%c", '\n');    
+            //ecriture des ecart types
+            fprintf(fiModel, "%s;", currentMovement);
+            for(int i = 0; i < TIME_FOR_A_MIN; i++){
+                double standartDeviation = data[i].totSquareVacc / data[i].nbLine - data[i].totVacc / data[i].nbLine;
+                fprintf(fiModel, "%lf;", standartDeviation);
+            }
+            fprintf(fiModel, "%c", '\n');
+            //écriture de la moyenne générale
+            fprintf(fiModel, "%s;", currentMovement);
+            fprintf(fiModel, "%lf;", totNbVacc / totMovement);
+            fprintf(fiModel, "%c", '\n');
+
+            fgets(line, sizeof(line), trainSet);
+        }
+
+        fclose(trainSet);
+        fclose(fiModel);
     }
+}
+
+void initFiModel(FILE* fiModel){
+    fprintf(fiModel, "%s;", "mouvement");
+    for(int i = 0; i < TIME_FOR_A_MIN; i++)
+        fprintf(fiModel, "%s;", "Vacc");
+    fprintf(fiModel, "%s", "\n");
 }
